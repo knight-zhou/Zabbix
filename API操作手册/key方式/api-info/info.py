505274a7cf895bc8b97d81fd4886143e0b1582eb
knight <624867243@qq.com>
2016-02-28 23:27:07 +0800

    。。。。。

---
+++ API操作手册/key方式/api-info/info.py
@@ -0,0 +1,29 @@
+#coding:utf-8
+import json
+import urllib2
+from _mysql import result
+url = "http://zabbix.knight.ren/api_jsonrpc.php"
+header = {"Content-Type": "application/json"}
+# request json
+data = json.dumps(
+{
+    "jsonrpc":"2.0",
+    "method":"apiinfo.version",
+    "params":[],
+    "auth":"8c67fd696eb3a6a877569b9bc34d6c22",
+    "id":1,
+})
+# create request object
+request = urllib2.Request(url,data)
+
+#print request
+for key in header:
+    request.add_header(key,header[key])
+    result = urllib2.urlopen(request)
+    for h in result:
+        print h
+
+
+
+
+
---
+++ API操作手册/key方式/api-info/readme.txt
@@ -0,0 +1,4 @@
+·ÂÕÕ£º
+https://www.zabbix.com/documentation/2.2/manual/api/reference/apiinfo/version
+
+Ğ´µÄÇëÇóÄÚÈİ
