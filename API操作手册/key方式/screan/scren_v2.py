3d2de690c9fe618a5e4d59099f21aa1a2c835cd2
knight <624867243@qq.com>
2016-02-29 00:12:59 +0800

    laod方法解码转换成原始数据

---
+++ API操作手册/key方式/screan/scren_v2.py
@@ -0,0 +1,41 @@
+#coding:utf-8
+#Check if a screen named “abc” already exists
+#result 
+import json
+import urllib2
+from _mysql import result
+url = "http://zabbix.knight.ren/api_jsonrpc.php"
+header = {"Content-Type": "application/json"}
+# request json
+data = json.dumps(
+{
+    "jsonrpc":"2.0",
+    "method":"screen.exists",
+    "params":{
+              "name":"abc"
+              },
+    "auth":"8c67fd696eb3a6a877569b9bc34d6c22",
+    "id":1,
+})
+# create request object
+request = urllib2.Request(url,data)
+
+#print request
+for key in header:
+    request.add_header(key,header[key])
+    result = urllib2.urlopen(request)
+    #对json格式解码
+    response = json.loads(result.read())
+    print response
+    print response['result']
+    
+   
+    
+          
+ 
+       
+
+
+
+
+
